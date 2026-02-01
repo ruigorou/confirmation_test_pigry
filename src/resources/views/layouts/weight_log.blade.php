2820416365b481c4cddad733039ea841d89e74c3
kai <kai5903@gmail.com>
2026-02-01 23:45:02 +0900

    icon

--- src/resources/views/layouts/weight_log.blade.php
+++ src/resources/views/layouts/weight_log.blade.php
@@ -6,6 +6,8 @@
     <title>PigLy</title>
     <link rel="stylesheet" href="{{ asset('css/sanitize.css') }}">
     <link rel="stylesheet" href="{{ asset('css/weight_log.css') }}">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
+
     @yield('css')
 </head>
 <body>
@@ -15,10 +17,11 @@
         </div>
         <div class="header__contents">
             <div class="header__target-weight">
-                <a href="">目標体重設定</a>
+                <a href=""><span><i class="fa-solid fa-gear"></i></span>目標体重設定</a>
             </div>
             <div class="header__logout">
-                <a href="/login">ログアウト</a>
+                <a href="/login"><span><i class="fa-solid fa-door-open"></i>
+</span>ログアウト</a>
             </div>
         </div>
     </header>
